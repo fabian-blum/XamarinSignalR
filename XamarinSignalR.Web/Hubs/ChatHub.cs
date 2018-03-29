a349120ddf8558b9e188c93b5f97a50a8a6cdf44
Fabian Blum <fabian-blum@outlook.de>
2018-03-29 19:30:48 +0200

    direct message to specific user

--- XamarinSignalR.Web/Hubs/ChatHub.cs
+++ XamarinSignalR.Web/Hubs/ChatHub.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace XamarinSignalR.Web.Hubs
@@ -10,7 +11,14 @@ namespace XamarinSignalR.Web.Hubs
         {
             string timestamp = DateTime.Now.ToShortTimeString();
 
-            return Clients.All.SendAsync("ReceiveMessage", timestamp, user, message);
+            Debug.WriteLine(Context.ConnectionId);
+            Debug.WriteLine(Context.Connection.ConnectionId);
+            Debug.WriteLine(Context.Connection.UserIdentifier);
+
+
+
+
+            return Clients.User("9bed5602-d7c4-4ec6-a0f0-165e6834d3da").SendAsync("ReceiveMessage", timestamp, user, message);
         }
     }
 }
